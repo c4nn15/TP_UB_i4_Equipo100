9a7997680afcacfb7d290bf33023cad64ff87a8a
tomasfigini-bit <tomas.figini@comunidad.ub.edu.ar>
2026-04-22 19:50:33 -0300

    Add subtract function test to calculator.test.js
    Added a test for the subtract function.

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,10 @@
-import { add } from './calculator';
+import { add, subtract } from './calculator.js';
 
 test('Alfa: add(1, 2) should return 3', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('Beta: subtract(5, 2) should return 3', () => {
+  // Esta prueba es correcta, pero fallará por culpa del bug en calculator.js
+  expect(subtract(5, 2)).toBe(3);
 });
