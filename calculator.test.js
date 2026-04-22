956fd89eaf51cefadc6fc152b91d52af20f20642
Francisco Peroni <franperoni30@gmail.com>
2026-04-22 20:53:20 -0300

    Gamma: fixed subtract bug detected by tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,17 @@
-import { add } from './calculator';
+import { add, subtract } from './calculator.js';
 
 test('Alfa: add(1, 2) should return 3', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('Gamma: subtract(5, 2) should return 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('Gamma: subtract(10, 4) should return 6', () => {
+  expect(subtract(10, 4)).toBe(6);
+});
+
+test('Gamma: subtract(0, 5) should return -5', () => {
+  expect(subtract(0, 5)).toBe(-5);
 });
