fbaeef265fe90d3279817272f04d234c846e7c19
Santiago Cannistra <133607520+c4nn15@users.noreply.github.com>
2026-04-24 19:04:46 -0300

    Agrego función de multiplicación
    Fix bug in multiply function that incorrectly adds 1 to the product.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
 export function add(a, b) {
   return a + b;
 }
+
 export function subtract(a, b) {
   return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b + 1; // El "+ 1" es el bug que introducís a propósito
 }
